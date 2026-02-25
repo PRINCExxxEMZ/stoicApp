555b96d14dacd0a6ffe87361b523e4df3da978ba
Adeoye Prince <63949240+PRINCExxxEMZ@users.noreply.github.com>
2026-02-25 14:23:07 +0100

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import prompts from "../data/prompts";
 import PromptCard from "../components/PromptCard";
 import WritingEditor from "../components/WritingEditor";
-import DownloadButton from "../components/DownloadButton";
 
 const Home = () => {
   const prompt = prompts[new Date().getDate() % prompts.length];
@@ -10,7 +9,6 @@ const Home = () => {
     <div className="max-w-2xl mx-auto px-4 py-12 space-y-8">
       <PromptCard prompt={prompt} />
       <WritingEditor />
-      {/* <DownloadButton/> */}
     </div>
   );
 };
